610a4f9d599a34eacea498fac23130d07bd5d0d6
Tom Coakes <tomcoakes@gmail.com>
2015-02-11 20:27:32 +0000

    Game class created and creates two players upon initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,10 @@
 class Game
+
+  attr_reader :player1, :player2
+
+  def initialize
+    @player1 = Player.new
+    @player2 = Player.new
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,10 @@
 require 'game'
 
-# describe Game do
-#   it "knows how many floating boats there are on the board" do
-#   end
-# end
+describe Game do
+
+  it "creates two players when the game is initialized" do
+    battleships = Game.new
+    expect(battleships.player1).to respond_to(:place_ship)
+    expect(battleships.player2).to respond_to(:place_ship)
+  end
+end
