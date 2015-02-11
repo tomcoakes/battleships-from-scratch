16394e41b13e816d726830e5b46dfc72d495c377
Tom Coakes <tomcoakes@gmail.com>
2015-02-11 12:33:02 +0000

    Board tells shots whether they were a hit or a miss when placed on the board

--- lib/board.rb
+++ lib/board.rb
@@ -12,7 +12,14 @@ class Board
   end
 
   def add_to_cell(y, x, obj)
+    register_hit_or_miss(y, x, obj)
     grid[y][x] = obj
+  end
+
+  def register_hit_or_miss(y, x, obj)
+    if obj.is_a?(Shot)
+      grid[y][x] == :w ? obj.register_miss : obj.register_hit
+    end
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,8 @@ require 'board'
 
 describe Board do
   let(:board) {Board.new}
+  let(:ship) {double :ship}
+  let(:shot) {double :shot, is_a?: :Shot}
 
   it "has a grid" do
     expect(board).to respond_to(:grid)
@@ -16,9 +18,23 @@ describe Board do
   end
 
   it "can add other objects to its cells" do
-    ship = double :ship
     board.add_to_cell(1, 0, ship)
     expect(board.grid).to eq([[:w,:w],[ship,:w]])
+  end
+
+  # it "can check what is currently in a cell before placement" do
+  #   board.add_to_cell
+  # end
+
+  it "tells a shot that it was a miss when placed in water" do
+    expect(shot).to receive(:register_miss) 
+    board.add_to_cell(0, 0, shot)
+  end
+
+  it "tells a shot that it was a hit when placed on a ship" do
+    board.add_to_cell(1, 0, ship)
+    expect(shot).to receive(:register_hit)
+    board.add_to_cell(1, 0, shot)
   end
 
 end
