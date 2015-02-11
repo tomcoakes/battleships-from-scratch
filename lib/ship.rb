21fa4398088945204a2b26fec3cc0d0128e9803f
Tom Coakes <tomcoakes@gmail.com>
2015-02-11 12:00:31 +0000

    Created Ship class methods for ships of different sizes. Also changed the way Ship deals with hits and sinking.

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,17 +1,49 @@
 class Ship
 
-  attr_reader :owner
+  attr_reader :size
 
-  def initialize(owner)
-    @owner = owner
+  def initialize(size)
+    @size = size
+    @hits = 0
   end
 
-  def register_hit
-    @destroyed = true
+  def hit
+    @hits += 1
   end
 
-  def destroyed?
-    @destroyed ||= false
+  def hits
+    @hits
+  end
+
+  def sunk?
+    hits >= size
+  end
+
+
+  # Class methods
+
+  def self.dinghy
+    new(1)
+  end
+
+  def self.patrol_boat
+    new(2)
+  end
+
+  def self.destroyer
+    new(3)
+  end
+
+  def self.submarine
+    new(3)
+  end
+
+  def self.battleship
+    new(4)
+  end
+
+  def self.aircraft_carrier
+    new(5)
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,17 +2,69 @@ require 'ship'
 
 describe Ship do
 
-  let(:player) {double :player}
+  let(:ship) {Ship.new(1)}
+  let(:large_ship) {Ship.new(2)}
 
-  it "is owned by a player" do
-    ship = Ship.new(player)
-    expect(ship.owner).to eq(player)
+  it "knows its size" do
+    expect(ship.size).to eq(1)
   end
 
-  it "can be destroyed" do
-    ship = Ship.new(player)
-    ship.register_hit
-    expect(ship).to be_destroyed
+  it "should know when it's been hit once" do
+    large_ship.hit
+    expect(large_ship.hits).to eq(1)
   end
+
+  it "should know when it's been hit twice" do
+    large_ship.hit
+    large_ship.hit
+    expect(large_ship.hits).to eq(2)
+  end
+
+  it "should not be sunk when created" do
+    expect(ship).not_to be_sunk
+  end
+
+  it "will sink if hit enough times" do
+    large_ship.hit
+    large_ship.hit
+    expect(large_ship).to be_sunk
+  end
+
+
+context "Ship sizes" do
+
+  it "knows that a dinghy has a size of 1" do
+    dinghy = Ship.dinghy
+    expect(dinghy.size).to eq(1)
+  end
+
+  it "knows that a patrol boat has a size of 2" do
+    patrol_boat = Ship.patrol_boat
+    expect(patrol_boat.size).to eq(2)
+  end
+
+  it "knows that a destroyer has a size of 3" do
+    destroyer = Ship.destroyer
+    expect(destroyer.size).to eq(3)
+  end
+
+  it "knows that a submarine has a size of 3" do
+    submarine = Ship.submarine
+    expect(submarine.size).to eq(3)
+  end
+
+  it "knows that a battleship has a size of 4" do
+    battleship = Ship.battleship
+    expect(battleship.size).to eq(4)
+  end
+
+  it "knows that an aircraft carrier has a size of 5" do
+    aircraft_carrier = Ship.aircraft_carrier
+    expect(aircraft_carrier.size).to eq(5)
+  end
+
+
+end
+
 
 end
