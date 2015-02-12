5d6db4d1d1dab9f4ad958452dfed489207567e8d
Tom Coakes <tomcoakes@gmail.com>
2015-02-12 11:12:21 +0000

    Game checks whether the player has won each time they take a shot

--- lib/game.rb
+++ lib/game.rb
@@ -9,12 +9,19 @@ class Game
 
   def player_shoots(player, y, x, shot)
     player.take_shot(y, x, shot, opponents_board(player))
+    check_if_winner(player)
   end
 
   def opponents_board(player)
     players = [player1, player2]
     players.each do |opponent|
       return opponent.board if opponent != player
+    end
+  end
+
+  def check_if_winner(player)
+    if player.board.ships_remaining == 0
+      "That was a hit, you've won!"
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,16 +2,28 @@ require 'game'
 
 describe Game do
 
+  let(:battleships) {Game.new}
+  let(:shot) {double :shot}
+
   it "creates two players when the game is initialized" do
-    battleships = Game.new
     expect(battleships.player1).to respond_to(:place_ship)
     expect(battleships.player2).to respond_to(:place_ship)
   end
 
-  it "can shoot an opponent's board" do
-    battleships = Game.new
-    shot = Shot.new
+  it "can pass a player's shot to the opponent's board" do
     expect(battleships.player2.board).to receive(:add_to_cell)
     battleships.player_shoots(battleships.player1, 0, 0, shot)
+  end
+
+  it "declares player 1 as the winner when player 2's ships are all sunk" do
+    battleships.player2.place_ship(0, 0, Ship.dinghy)
+    battleships.player_shoots(battleships.player1, 0, 0, Shot.new)
+    expect(battleships.check_if_winner(battleships.player1)).to eq("That was a hit, you've won!")
+  end
+
+  it "declares player 2 as the winner when player 1's ships are all sunk" do
+    battleships.player1.place_ship(0, 0, Ship.dinghy)
+    battleships.player_shoots(battleships.player2, 0, 0, Shot.new)
+    expect(battleships.check_if_winner(battleships.player2)).to eq("That was a hit, you've won!")
   end
 end
