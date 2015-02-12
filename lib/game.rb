7d9eba1fb43ea8f872ffddbb7c7a3e5599564e70
Tom Coakes <tomcoakes@gmail.com>
2015-02-12 09:33:26 +0000

    Game deals with player shooting and passes the shot from the one player to the other player's board

--- lib/game.rb
+++ lib/game.rb
@@ -7,4 +7,15 @@ class Game
     @player2 = Player.new
   end
 
+  def player_shoots(player, y, x, shot)
+    player.take_shot(y, x, shot, opponents_board(player))
+  end
+
+  def opponents_board(player)
+    players = [player1, player2]
+    players.each do |opponent|
+      return opponent.board if opponent != player
+    end
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,11 @@ describe Game do
     expect(battleships.player1).to respond_to(:place_ship)
     expect(battleships.player2).to respond_to(:place_ship)
   end
+
+  it "can shoot an opponent's board" do
+    battleships = Game.new
+    shot = Shot.new
+    expect(battleships.player2.board).to receive(:add_to_cell)
+    battleships.player_shoots(battleships.player1, 0, 0, shot)
+  end
 end
